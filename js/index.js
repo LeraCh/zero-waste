a09917a1eee5382ec6e7df05b9c5291b64e61611
LeraCh <v.chornovol.prgrf@gmail.com>
2020-04-17 09:11:14 +0000

    add smooth scroll

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// const ecoproducts = document.getElementById('ecoshop');
+// ecoproducts.addEventListener('click', function(){
+//     const products = $('#ecoshop-block').position().top;
+
+//     $('html, body').animate({
+//         scrollTop: products
+//     },
+//     900
+//     );    
+// });
+
+var scroll = new SmoothScroll('a[href*="#"]');
